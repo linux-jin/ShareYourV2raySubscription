1b15389465f5160e1df331832c2ebc48f830ddfe
LiJin <jin.l@medicalsystem.cn>
2023-02-23 17:02:58 +0800

    fix: fix

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 
 // configuration
 $url = 'index.php';
+$shareurl = 'sub.php';
 $file = 'sub.txt';
 $file_premium = 'sub2.txt';
+$file_ncm = 'ncm.txt';
 $key = 'key.txt';
 $key_md5 = 'md5.txt';
-$shareurl = 'sub.php';
 
 function removeComment($content){
         // (\/\*.*\*\/)|(#.*?\n)|(\/\/.*?\n)|
@@ -86,10 +87,21 @@ if (isset($_POST['text_premium']))
     printf('<a href="%s">Moved</a>.', htmlspecialchars($url));
     exit();
 }
+if (isset($_POST['text_ncm']))
+{
+    // save the text contents
+    file_put_contents($file_ncm, autoremarks($_POST['text_ncm']));
+
+    // redirect to form again
+    header(sprintf('Location: %s', $url));
+    printf('<a href="%s">Moved</a>.', htmlspecialchars($url));
+    exit();
+}
 
 // read the textfile
 $text = file_get_contents($file);
 $text_premium = file_get_contents($file_premium);
+$text_ncm = file_get_contents($file_ncm);
 $text_key = file_get_contents($key);
 $text_key_md5 = file_get_contents($key_md5);
 
@@ -116,123 +128,174 @@ $text_key_md5 = file_get_contents($key_md5);
 </head>
 
 <body>
-    <div class="container">
-        <div class="page-header"><!--标题部分---->
-            <h2>公共订阅编辑器</h2>
+<div class="container">
+    <!--标题部分---->
+    <div class="page-header">
+        <h2>公共订阅编辑器</h2>
+    </div>
+
+    <p>支持 VMESS 和 SS 分享链接。每行放一个链接。每次保存,自动解析生成每条分享链接对应的别名</p>
+
+    <!--文本框部分---->
+    <form action="" method="post">
+        <div class ="form-group">
+            <!--<textarea name="text" type="text" style="width:600px;height:400px;overflow-x:visible;overflow-y:visible;">-->
+            <label for="accesskey">分享密钥</label>
+            <input name="accesskey" id="accesskey" type="text" class="form-control" value="<?php echo htmlspecialchars($text_key); ?>"></input>
+            <div id="emailHelp" class="form-text">密钥以MD5形式保存,如果忘记可以在本页面重设</div>
+            <br>
+            <input class="form-check-input" type="checkbox" value="yes" name="dontsavekeytf" id="savekeytf" />
+            <label class="form-check-label" for="savekeytf">不明文保存密码(勾选并保存将清除以前明文存储的密码）</label>
+            <br>
+            <button type="submit" class="btn btn-primary" name="action" onclick=""/>立即保存</button>
+            <input type="reset" class="btn btn-default"/>
+            <button type="button" class="btn btn-success" onclick="generatesharelink()">生成分享链接</button>
+            <button type="button" class="btn btn-success" onclick="generatepwd()">一键生成密钥</button>
         </div>
-        <p>支持 VMESS 和 SS 分享链接。每行放一个链接。每次保存,自动解析生成每条分享链接对应的别名</p>
+    </form>
 
-        <form action="" method="post"><!--文本框部分---->
+    <br><br><br>
+
+    <form class="form-horizontal">
+        <div class ="form-group">
+            <!--<label for="sharelink_premium">分享链接（Premium）</label>-->
+            <div class="col-sm-8">
+                <input name="sharelink_premium" id="sharelink_premium" type="text" class="form-control" value="<?php if($text_key == ""){echo "";}else{echo $shareurl."?key=".$text_key."&level=premium";}; ?>"/>
+            </div>
+            <div class="col-sm-4"><button type="button" class="btn btn-success" onclick="copysharelink()">复制Premium线路 分享链接</button></div>
+        </div>
+        <div class ="form-group">
+            <div class="col-sm-8">
+                <input name="sharelink" id="sharelink" type="text" class="form-control" value="<?php if($text_key == ""){echo "";}else{echo $shareurl."?key=".$text_key;}; ?>"></input>
+            </div>
+            <div class="col-sm-4"><button type="button" class="btn btn-success" onclick="copysharelink2()">复制普通线路 分享链接</button></div>
+        </div>
+        <div class ="form-group">
+            <div class="col-sm-8">
+                <input name="sharelink_ncm" id="sharelink_ncm" type="text" class="form-control" value="<?php if($text_key == ""){echo "";}else{echo $shareurl."?key=".$text_key."&level=ncm";}; ?>"/>
+            </div>
+            <div class="col-sm-4"><button type="button" class="btn btn-success" onclick="copysharelinkncm()">复制NCM线路 分享链接</button></div>
+        </div>
+    </form>
+
+    <script>
+        function generatesharelink() {
+            /* Get the text field */
+            var url='<?php echo htmlspecialchars($shareurl)?>';
+            document.getElementById('sharelink_premium').value = url + '?key=' + document.getElementById('accesskey').value + '&level=premium';
+            document.getElementById('sharelink').value = url + '?key=' + document.getElementById('accesskey').value;
+            document.getElementById('sharelink_ncm').value = url + '?key=' + document.getElementById('accesskey').value  + '&level=ncm';
+        };
+
+        function copysharelink() {
+            var copyText = document.getElementById('sharelink_premium');
+
+            /* Select the text field */
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+            /* Copy the text inside the text field */
+            navigator.clipboard.writeText(window.location.origin + '/' + copyText.value);
+
+            /* Alert the copied text */
+            alert("Copied the text: " + copyText.value+ "【请别忘记点击保存】");
+        };
+
+        function copysharelink2() {
+            var copyText = document.getElementById('sharelink');
+
+            /* Select the text field */
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); /* For mobile devices */
+            /* Copy the text inside the text field */
+            navigator.clipboard.writeText(window.location.origin + '/' + copyText.value);
+
+            /* Alert the copied text */
+            alert("Copied the text: " + copyText.value + "【请别忘记点击保存】");
+        };
+
+
+        function copysharelinkncm() {
+            var copyText = document.getElementById('sharelink_ncm');
+
+            /* Select the text field */
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); /* For mobile devices */
+            /* Copy the text inside the text field */
+            navigator.clipboard.writeText(window.location.origin + '/' + copyText.value);
+
+            /* Alert the copied text */
+            alert("Copied the text: " + copyText.value + "【请别忘记点击保存】");
+        };
+
+        function generatepwd() {
+                var pasArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
+                var password = '';
+                var pasArrLen = pasArr.length;
+                for (var i=0; i<16; i++){
+                    var x = Math.floor(Math.random()*pasArrLen);
+                    password += pasArr[x];
+                }
+                document.getElementById('accesskey').value = password;
+                generatesharelink();
+            };
+
+    </script>
+
+
+    <br><br><br>
+
+    <!--普通线路文本框部分---->
+    <div ><h3>Premium 线路</h3></div>
+    <div>
+        <form action="" method="post">
             <div class ="form-group">
-                        <!--<textarea name="text" type="text" style="width:600px;height:400px;overflow-x:visible;overflow-y:visible;">-->
-                        <label for="accesskey">分享密钥</label>
-                        <input name="accesskey" id="accesskey" type="text" class="form-control" value="<?php echo htmlspecialchars($text_key); ?>"></input>
-                        <div id="emailHelp" class="form-text">密钥以MD5形式保存,如果忘记可以在本页面重设</div>
-                        <br><input class="form-check-input" type="checkbox" value="yes" name="dontsavekeytf" id="savekeytf" />
-                        <label class="form-check-label" for="savekeytf">不明文保存密码(勾选并保存将清除以前明文存储的密码）</label>
-
-                        <br>
-                <button type="submit" class="btn btn-primary" name="action" onclick=""/>立即保存</button>
-                <input type="reset" class="btn btn-default"/>
-                <button type="button" class="btn btn-success" onclick="generatesharelink()">生成分享链接</button>
-                <button type="button" class="btn btn-success" onclick="generatepwd()">一键生成密钥</button>
-                </form>
-                <br><br>
-                <form class="form-horizontal">
-                    <div class ="form-group">
-                        <!--<label for="sharelink_premium">分享链接（Premium）</label>-->
-                    <div class="col-sm-8">
-                        <input name="sharelink_premium" id="sharelink_premium" type="text" class="form-control" value="<?php if($text_key == ""){echo "";}else{echo $shareurl."?key=".$text_key."&level=premium";}; ?>"/>
-                    </div>
-                    <div class="col-sm-4"><button type="button" class="btn btn-success" onclick="copysharelink()">复制Premium分享链接</button></div>
-                </div>
-                    <div class ="form-group">
-                        <!--<label for="sharelink">分享链接（普通）</label>-->
-                    <div class="col-sm-8">
-                        <input name="sharelink" id="sharelink" type="text" class="form-control" value="<?php if($text_key == ""){echo "";}else{echo $shareurl."?key=".$text_key;}; ?>"></input>
-                    </div>
-                    <div class="col-sm-4"><button type="button" class="btn btn-success" onclick="copysharelink2()">复制普通线路分享链接</button></div>
-                </div>
-                </form>
-                <script>
-                    function generatesharelink() {
-                        /* Get the text field */
-                        var url='<?php echo htmlspecialchars($shareurl)?>';
-                        document.getElementById('sharelink_premium').value = url + '?key=' + document.getElementById('accesskey').value + '&level=premium';
-                        document.getElementById('sharelink').value = url + '?key=' + document.getElementById('accesskey').value;
-                    };
-
-                    function copysharelink() {
-                        var copyText = document.getElementById('sharelink_premium');
-
-                        /* Select the text field */
-                        copyText.select();
-                        copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-                        /* Copy the text inside the text field */
-                        navigator.clipboard.writeText(window.location.origin + '/' + copyText.value);
-
-                        /* Alert the copied text */
-                        alert("Copied the text: " + copyText.value+ "【请别忘记点击保存】");
-                        };
-
-                    function copysharelink2() {
-                        var copyText = document.getElementById('sharelink');
-
-                        /* Select the text field */
-                        copyText.select();
-                        copyText.setSelectionRange(0, 99999); /* For mobile devices */
-                        /* Copy the text inside the text field */
-                        navigator.clipboard.writeText(window.location.origin + '/' + copyText.value);
-
-                        /* Alert the copied text */
-                        alert("Copied the text: " + copyText.value + "【请别忘记点击保存】");
-                        };
-
-                    function generatepwd() {
-                            var pasArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
-                            var password = '';
-                            var pasArrLen = pasArr.length;
-                            for (var i=0; i<16; i++){
-                                var x = Math.floor(Math.random()*pasArrLen);
-                                password += pasArr[x];
-                            }
-                            document.getElementById('accesskey').value = password;
-                            generatesharelink();
-                        };
-
-                    </script>
-
-            </div><br><br>
-
-            <form action="" method="post"><!--文本框部分---->
-                <div  class ="form-group">
-                    <h3>Premium 线路</h3>
-                    <div>
-                        <!--<textarea name="text" type="text" style="width:600px;height:400px;overflow-x:visible;overflow-y:visible;">-->
-                        <textarea name="text_premium" id="text_premium" type="text" class="form-control"  rows="18"><?php echo htmlspecialchars($text_premium) ?></textarea>
-                         <br>
-                    </div>
-                </div>
-                <input type="submit" class="btn btn-primary" name="action" onclick="savev2ray_premium();"/>
-                <input type="reset" class="btn btn-default"/>
-            </form><br><br><br>
-        <div ><h3>普通线路</h3></div>
-            <div>
-                <form action="" method="post">
-                    <div class ="form-group">
-                        <!--<textarea name="text" type="text" style="width:600px;height:400px;overflow-x:visible;overflow-y:visible;">-->
-                        <textarea name="text" id="text" type="text" class="form-control"  rows="18"><?php echo htmlspecialchars($text) ?></textarea>
-                         <br>
-                    </div>
-                <input type="submit" class="btn btn-primary" name="action2" onclick="savev2ray();"/>
-                <input type="reset" class="btn btn-default"/>
-                </form>
-            </div><br>
+                <!--<textarea name="text" type="text" style="width:600px;height:400px;overflow-x:visible;overflow-y:visible;">-->
+                <textarea name="text_premium" id="text_premium" type="text" class="form-control"  rows="18"><?php echo htmlspecialchars($text_premium) ?></textarea>
+                <br>
             </div>
-            <footer class="footer"><!--脚注部分---->
-            <div class="container">
-                <p class="text-muted">Help is on the way.</p>
+            <input type="submit" class="btn btn-primary" name="action" onclick="savev2ray_premium();"/>
+            <input type="reset" class="btn btn-default"/>
+        </form>
+    </div>
+
+    <br><br><br>
+
+    <!--普通线路文本框部分---->
+    <div ><h3>普通 线路</h3></div>
+    <div>
+        <form action="" method="post">
+            <div class ="form-group">
+                <!--<textarea name="text" type="text" style="width:600px;height:400px;overflow-x:visible;overflow-y:visible;">-->
+                <textarea name="text" id="text" type="text" class="form-control"  rows="18"><?php echo htmlspecialchars($text) ?></textarea>
+                <br>
             </div>
-        </footer>
+            <input type="submit" class="btn btn-primary" name="action2" onclick="savev2ray();"/>
+            <input type="reset" class="btn btn-default"/>
+        </form>
+    </div>
+
+    <br><br><br>
+
+    <!--普通线路文本框部分---->
+    <div ><h3>NCM 线路</h3></div>
+    <div>
+        <form action="" method="post">
+            <div class ="form-group">
+                <!--<textarea name="text" type="text" style="width:600px;height:400px;overflow-x:visible;overflow-y:visible;">-->
+                <textarea name="text_ncm" id="text_ncm" type="text" class="form-control"  rows="18"><?php echo htmlspecialchars($text_ncm) ?></textarea>
+                <br>
+            </div>
+            <input type="submit" class="btn btn-primary" name="action" onclick="savev2ray_ncm();"/>
+            <input type="reset" class="btn btn-default"/>
+        </form>
+    </div>
+</div>
+
+<!--脚注部分---->
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">Help is on the way.</p>
+    </div>
+</footer>
 </body>
--- sub.php
+++ sub.php
@@ -2,11 +2,11 @@
     $key_md5 = 'md5.txt';//在这里填写存储AccessKey MD5文件的绝对路径，例如 /www/example.com/md5.txt
     $file = 'sub.txt';//在这里填写存储普通线路订阅源文件的绝对路径，例如 /www/example.com/sub.txt
     $file_premium = 'sub2.txt';//在这里填写存储 premium 线路订阅源文件的绝对路径，例如 /www/example.com/sub2.txt
+    $file_ncm = 'ncm.txt';//在这里填写存储 ncm 线路订阅源文件的绝对路径，例如 /www/example.com/ncm.txt
 
     function removeComment($content){
         // (\/\*.*\*\/)|(#.*?\n)|(\/\/.*?\n)|
         return preg_replace("/(<!--[\w\W\r\n]*?-->)/s", '', str_replace(array("\r\n", "\r"), "\n", $content));
-
     };
     function removeVmess($content){
         // (\/\*.*\*\/)|(#.*?\n)|(\/\/.*?\n)|
@@ -30,16 +30,20 @@
     if (($key_md5_number == md5($key_from_url["key"])) or ($key_md5_number == "")) {//判断url中是否传入key
         if (strpos(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), 'level=premium')!==false) {
            $str = file_get_contents($file_premium, "r") or die("Unable to open file!");
-        } else {
+        }
+        else if (strpos(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), 'level=ncm')!==false) {
+            $str = file_get_contents($file_ncm, "r") or die("Unable to open file!");
+        }
+        else {
             $str = file_get_contents($file, "r") or die("Unable to open file!");
         }
-    
+
         if (strpos(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), 'type=ss')!==false) {
            echo base64_encode(removeVmess(removeVless(removeComment($str))));
         }
         else if (strpos(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), 'type=vmess')!==false) {
            echo base64_encode(removeSS(removeVless(removeComment($str))));
-        } 
+        }
         else if (strpos(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), 'type=vless')!==false) {
             echo base64_encode(removeSS(removeVmess(removeComment($str))));
         }
