f2a1daf8de99b5ba8d6e409e7eb541d6a0e9f22b
beer-on-ice <li997477295@outlook.com>
2024-01-03 10:12:28 +0800

    feat: test vercel

--- sub.php
+++ sub.php
@@ -4,6 +4,7 @@
     $file_premium = 'sub2.txt';//在这里填写存储 premium 线路订阅源文件的绝对路径，例如 /www/example.com/sub2.txt
     $file_ncm = 'ncm.txt';//在这里填写存储 ncm 线路订阅源文件的绝对路径，例如 /www/example.com/ncm.txt
 
+
     function removeComment($content){
         // (\/\*.*\*\/)|(#.*?\n)|(\/\/.*?\n)|
         return preg_replace("/(<!--[\w\W\r\n]*?-->)/s", '', str_replace(array("\r\n", "\r"), "\n", $content));
@@ -32,13 +33,16 @@
         // (\/\*.*\*\/)|(#.*?\n)|(\/\/.*?\n)|
         return preg_replace("~\nnaive\+https://.*~", '', $content);
     };
+
     $url_query = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
     if(strpos($url_query, 'key')!==false){
         parse_str($url_query, $key_from_url);
     } else {
         $key_from_url = array("key"=>"");
     };
+
     $key_md5_number = file_get_contents($key_md5, "r");
+
     if (($key_md5_number == md5($key_from_url["key"])) or ($key_md5_number == "")) {//判断url中是否传入key
         if (strpos(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), 'level=premium')!==false) {
            $str = file_get_contents($file_premium, "r") or die("Unable to open file!");
